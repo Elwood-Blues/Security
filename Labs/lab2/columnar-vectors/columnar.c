7f83383f000f5b21d972aaf8ea092b8cdcd381f1
Peter Braband <braba006@d.umn.edu>
2017-02-01 21:30:48 -0600

    all functions written, needs debugging still

--- Labs/lab2/columnar-vectors/columnar.c
+++ Labs/lab2/columnar-vectors/columnar.c
@@ -228,6 +228,27 @@ int main(int argc, char *argv[]) {
 	/******************
 	 *  do stuff here *
 	 ******************/
+	i = 0;
+	while((bytesleft > 0){
+		if((symbol = fgetc(INPUT)) != EOF){
+			if (((rbuf_index % bufsize) == 0) && i != 0)
+			{
+				/* read buffer if full, transpose and call dump buffer to write the block */
+				transpose_buffer(write_buf, read_buf, bufsize);
+				dump_buffer(read_buf, bufsize, bufsize, write_buf);
+			}
+			read_buf[rbuf_index % bufsize] = symbol;
+			rbuf_index++;
+			bytesleft--;
+			i++;
+		}
+		else{
+			//reached the EOF signal, pad buffer, then transpose and finally dump buffer
+			pad_buffer(read_buf, bufsize, (rbuf_index % bufsize));
+			transpose_buffer(write_buf, read_buf, bufsize);
+			dump_buffer(read_buf, bufsize, (rbuf_index % bufsize), write_buf);
+		}
+	}
 
 	fclose(INPUT);
 
